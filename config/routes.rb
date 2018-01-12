6330d8bf2a8bf7d3c1f3500bff090f637c467915
山木祥平 <yamaki817122@gmail.com>
2018-01-12 14:40:48 +0900

    自動入力可能化

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'addresses/new'
+  root 'addresses#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
